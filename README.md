27192feeb5585e82fb8edcfcdbb4e73cd27016fc
Rafael Ostertag <rafi@guengel.ch>
2018-07-14 18:36:06 +0200

    Add support for log file

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ The service is configured via environment variables.
 
 * `NMAP_CMD`: file path of the nmap executable. Defaults to `nmap`.
 * `LISTEN`: address and port to listen on. This value is passed to `http.ListenAndServe()`. Defaults to `:8081`.
+* `LOGPATH`: path to log directory. When set, logs are written to this directory.
--- src/guengel.ch/services/nmapservice/service.go
+++ src/guengel.ch/services/nmapservice/service.go
@@ -4,12 +4,15 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/go-ozzo/ozzo-routing"
 	"github.com/go-ozzo/ozzo-routing/access"
 	"github.com/go-ozzo/ozzo-routing/content"
 	"github.com/go-ozzo/ozzo-routing/fault"
 	"github.com/go-ozzo/ozzo-routing/slash"
+	"gopkg.in/natefinch/lumberjack.v2"
 	"guengel.ch/net/nmap"
 )
 
@@ -32,7 +35,37 @@ func getListenAddress() string {
 	return listen
 }
 
+func setUpLogging() {
+	logpath, isLogPathSet := os.LookupEnv("LOGPATH")
+	if isLogPathSet == false {
+		log.Print("Logging to stdout")
+		return
+	}
+
+	lj := lumberjack.Logger{
+		Filename:   logpath + "/nmapservice.log",
+		MaxSize:    500, // megabytes
+		MaxBackups: 3,
+		MaxAge:     28,   //days
+		Compress:   true, // disabled by default
+	}
+	log.SetOutput(&lj)
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGHUP)
+
+	go func() {
+		for {
+			<-c
+			lj.Rotate()
+			log.Print("Rotated log")
+		}
+	}()
+}
+
 func main() {
+	setUpLogging()
+
 	router := routing.New()
 
 	router.Use(
