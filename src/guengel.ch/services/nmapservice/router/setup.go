eb9d8db9525e120da299ae036ad50549ef0bb723
Rafael Ostertag <rafi@guengel.ch>
2018-08-01 18:21:33 +0200

    Factor out routing and handlers

---
+++ src/guengel.ch/services/nmapservice/router/setup.go
@@ -0,0 +1,56 @@
+package router
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-ozzo/ozzo-routing"
+	"github.com/go-ozzo/ozzo-routing/access"
+	"github.com/go-ozzo/ozzo-routing/content"
+	"github.com/go-ozzo/ozzo-routing/fault"
+	"github.com/go-ozzo/ozzo-routing/slash"
+
+	"guengel.ch/net/nmap"
+)
+
+func handleScanRequest(c *routing.Context) error {
+	var err error
+	var result *nmap.Result
+
+	if result, err = nmap.Run(c.Param("host"), c.Param("portSpec")); err != nil {
+		return err
+	}
+
+	return c.Write(result)
+}
+
+func handleHealthRequest(c *routing.Context) error {
+	type HealthStruct struct {
+		Status string
+	}
+
+	health := HealthStruct{"ok"}
+
+	return c.Write(health)
+}
+
+// ApplicationRouting sets up the router for the application
+func ApplicationRouting() *routing.Router {
+	router := routing.New()
+
+	router.Use(
+		access.Logger(log.Printf),
+		slash.Remover(http.StatusMovedPermanently),
+		fault.Recovery(log.Printf),
+		content.TypeNegotiator(content.JSON),
+	)
+
+	router.Get("/health", handleHealthRequest)
+
+	api := router.Group("/v1")
+
+	api.Get("/scan/<host:[a-zA-Z0-9.-]+>/<portSpec:[\\d,-]+>", handleScanRequest)
+	api.Get("/scan/<host:[a-zA-Z0-9.-]+>", handleScanRequest)
+
+	return router
+}
--- src/guengel.ch/services/nmapservice/service.go
+++ src/guengel.ch/services/nmapservice/service.go
@@ -7,35 +7,10 @@ import (
 	"os/signal"
 	"syscall"
 
-	"github.com/go-ozzo/ozzo-routing"
-	"github.com/go-ozzo/ozzo-routing/access"
-	"github.com/go-ozzo/ozzo-routing/content"
-	"github.com/go-ozzo/ozzo-routing/fault"
-	"github.com/go-ozzo/ozzo-routing/slash"
+	"guengel.ch/services/nmapservice/router" 
+
 	"gopkg.in/natefinch/lumberjack.v2"
-	"guengel.ch/net/nmap"
 )
-
-func handleScanRequest(c *routing.Context) error {
-	var err error
-	var result *nmap.Result
-
-	if result, err = nmap.Run(c.Param("host"), c.Param("portSpec")); err != nil {
-		return err
-	}
-
-	return c.Write(result)
-}
-
-func handleHealthRequest(c *routing.Context) error {
-	type HealthStruct struct {
-		Status string `json:"status"`
-	}
-
-	health := HealthStruct{"ok"}
-
-	return c.Write(health)
-}
 
 func getListenAddress() string {
 	listen := os.Getenv("LISTEN")
@@ -76,23 +51,7 @@ func setUpLogging() {
 func main() {
 	setUpLogging()
 
-	router := routing.New()
-
-	router.Use(
-		access.Logger(log.Printf),
-		slash.Remover(http.StatusMovedPermanently),
-		fault.Recovery(log.Printf),
-		content.TypeNegotiator(content.JSON),
-	)
-
-	router.Get("/health", handleHealthRequest)
-
-	api := router.Group("/v1")
-
-	api.Get("/scan/<host:[a-zA-Z0-9.-]+>/<portSpec:[\\d,-]+>", handleScanRequest)
-	api.Get("/scan/<host:[a-zA-Z0-9.-]+>", handleScanRequest)
-
-	http.Handle("/", router)
+	http.Handle("/", router.ApplicationRouting())
 	var listenAddress = getListenAddress()
 	log.Printf("Starting server on %s", listenAddress)
 	log.Fatal(http.ListenAndServe(listenAddress, nil))
